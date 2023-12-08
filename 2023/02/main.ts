2a1a7bed5881b81014685f1756e17be4c5cf16ec
Tim Altrichter <timaltrichter@gmail.com>
2023-12-08 22:43:00 +0100

    Completed Day 2 Part 2

--- 2023/02/main.ts
+++ 2023/02/main.ts
@@ -32,7 +32,7 @@ function checkIfGameisPossible(game: Array<Map<string, string>>): boolean {
 	});
 }
 
-function part1(inputArray: Array<string>) {
+function part1(inputArray: Array<string>): void {
 	const formattedGames: Map<number, Array<Map<string, string>>> = getFormattedGames(inputArray);
 	let result = 0;
 	formattedGames.forEach((value: Array<Map<string, string>>, key: number) => {
@@ -40,7 +40,34 @@ function part1(inputArray: Array<string>) {
 			result += key;
 		}
 	});
-	console.log(result);
+	console.log('Part 1: ' + result);
+}
+
+function getSumOfHighestDicePowers(game: Array<Map<string, string>>): number {
+	const highestDicePower: { [key: string]: number } = {
+		blue: 0,
+		green: 0,
+		red: 0,
+	};
+
+	game.map((value: Map<string, string>) => {
+		for (const color in highestDicePower) {
+			if (value.has(color) && parseInt(value.get(color)!) > highestDicePower[color]) {
+				highestDicePower[color] = parseInt(value.get(color)!);
+			}
+		}
+	});
+
+	return Object.values(highestDicePower).reduce((a, b) => a * b);
+}
+
+function part2(inputArray: Array<string>): void {
+	const formattedGames: Map<number, Array<Map<string, string>>> = getFormattedGames(inputArray);
+	let result = 0;
+	formattedGames.forEach((value: Array<Map<string, string>>) => {
+		result += getSumOfHighestDicePowers(value);
+	});
+	console.log('Part 2: ' + result);
 }
 
 const part1LoadedDice: { [key: string]: number } = {
@@ -53,5 +80,5 @@ if (import.meta.main) {
 	const inputArray: Array<string> = getInput().split('\n');
 
 	part1(inputArray);
-	// part2(inputArray);
+	part2(inputArray);
 }
